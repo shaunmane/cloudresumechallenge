c9efb0ba8651c3983dee50d5aee7a046ca323b7d
Shaun <131450123+shaunmane@users.noreply.github.com>
2026-06-06 20:33:05 +0000

    added api gateway

---
+++ api_gateway.tf
@@ -0,0 +1,46 @@
+
+resource "aws_api_gateway_rest_api" "counter_api" {
+  body = jsonencode({
+    openapi = "3.0.1"
+    info = {
+      title   = var.rest_api_name
+      version = "1.0"
+    }
+    paths = {
+      (var.rest_api_path) = {
+        get = {
+          x-amazon-apigateway-integration = {
+            httpMethod           = "POST"
+            payloadFormatVersion = "1.0"
+            type                 = "AWS_PROXY"
+            uri                  = aws_lambda_function.create_function.invoke_arn
+          }
+        }
+      }
+    }
+  })
+
+  name = var.rest_api_name
+
+  endpoint_configuration {
+    types = ["REGIONAL"]
+  }
+}
+
+resource "aws_api_gateway_deployment" "counter_api_deployment" {
+  rest_api_id = aws_api_gateway_rest_api.counter_api.id
+
+  triggers = {
+    redeployment = sha1(jsonencode(aws_api_gateway_rest_api.counter_api.body))
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_api_gateway_stage" "counter_api_stage" {
+  deployment_id = aws_api_gateway_deployment.counter_api_deployment.id
+  rest_api_id   = aws_api_gateway_rest_api.counter_api.id
+  stage_name    = var.counter_api_stage_name
+}
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -12,3 +12,16 @@ variable "bucket_name" {
   type = string
   default = "mywebsite"
 }
+
+variable "rest_api_name" {
+  type = string
+}
+
+variable "rest_api_path" {
+  type =string
+  default = "/"
+}
+
+variable "counter_api_stage_name" {
+  type = string
+}
